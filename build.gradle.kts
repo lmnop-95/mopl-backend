0d29356eb8074616167088174ff52de551784be4
JiseokBak <lmnop95x@gmail.com>
2026-02-10 22:49:46 +0900

    chore(sonar): remove SonarCloud integration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,3 @@
-plugins {
-    id("mopl.sonarqube-conventions")
-}
-
 listOf("applications", "core", "shared", "infrastructure").forEach { name ->
     project(name) {
         tasks.configureEach { enabled = false }
